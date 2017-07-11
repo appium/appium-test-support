52d64ae04dd1cc4c14ac2abf6acaba3750ce89c2
dan@saucelabs.com <dan@saucelabs.com>
2017-07-11 14:37:04 -0700

    Refactored TestObject API to remove mocha before/after
    * Removed the 'usingTestObject' function which made use of mocha hooks. The mocha hooks were a bit overengineered and difficult to debug/reason-about. In it's place, implemented enableTestObject and disableTestObject
    * enableTestObject uploads the appium zip and overrides WD so that it appends the TestObject specific capabilities to new Appium session (this should be called in mocha -> before)
    * disableTestObject takes the object returned by enableTestObject and uses it to restore WD and delete the Zip from S3 (this should be called in mocha -> after)
    * Refactored s3.js to look more look testObject.js
    * Added more logging

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { stubEnv } from './lib/env-utils.js';
 import { stubLog } from './lib/log-utils.js';
 import { fakeTime } from './lib/time-utils.js';
 import { withSandbox, withMocks, verify } from './lib/sandox-utils.js';
-import { usingTestObject, addTestObjectMochaHooks, getTestObjectCaps } from './lib/testobject.js';
+import { enableTestObject, disableTestObject } from './lib/testobject.js';
 
 export { stubEnv, stubLog, fakeTime, withSandbox, withMocks, verify, 
-  usingTestObject, addTestObjectMochaHooks, getTestObjectCaps };
+  enableTestObject, disableTestObject };
