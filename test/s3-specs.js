c3b2b6a23c4e5c51a8cd0b016fd8a51f5c12e787
dan@saucelabs.com <dan@saucelabs.com>
2017-09-08 11:12:22 -0700

    Re-use zips that are uploaded to S3
    * Checks if file already exists prior to creating new one
    * Don't re-zip and re-install new Appium if one is already saved to S3
    * Use `s3.headObject` to check if file exists (much quicker than requesting the whole thing)
    * Fix tests

--- test/s3-specs.js
+++ test/s3-specs.js
@@ -1,13 +1,16 @@
 // transpile:mocha
 /* eslint-disable promise/prefer-await-to-callbacks */
 
-import { uploadZip, deleteZip } from '../lib/s3';
+import S3 from '../lib/s3';
 
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import sinon from 'sinon';
 import AWS from 'aws-sdk';
 import { fs } from 'appium-support';
+
+const {uploadZip} = S3;
+
 chai.should();
 chai.use(chaiAsPromised);
 
@@ -15,14 +18,18 @@ describe('s3', () => {
   let s3Proto = Object.getPrototypeOf(new AWS.S3());
 
   describe('uploadZip', () => {
-    let fileExistsStub, readFileStub;
+    let fileExistsStub, readFileStub, uploaderStub, s3FileExistsStub;
     beforeEach(() => {
-      fileExistsStub = sinon.stub(fs, 'exists', () => true);
-      readFileStub = sinon.stub(fs, 'readFile', () => "dummy");
+      fileExistsStub = sinon.stub(fs, 'exists');
+      readFileStub = sinon.stub(fs, 'readFile');
+      s3FileExistsStub = sinon.stub(S3, 'fileExists');
+      readFileStub.returns('dummy');
+      fileExistsStub.returns(true);
     });
     afterEach(() => {
       fileExistsStub.restore();
       readFileStub.restore();
+      s3FileExistsStub.restore();
     });
     it('should reject if AWS_S3_BUCKET not defined in env', async () => {
       const backupBucket = process.env.AWS_S3_BUCKET;
@@ -32,37 +39,52 @@ describe('s3', () => {
     });
     it('should reject if file does not exist', async () => {
       fileExistsStub.restore();
-      fileExistsStub = sinon.stub(fs, 'exists', () => false);
+      fileExistsStub.returns(false);
+      const fakeEventObject = {
+        on: () => {},
+      };
+      uploaderStub = sinon.stub(s3Proto, 'upload', ((obj, cb) => {
+        cb('Could not find');
+        return fakeEventObject;
+      }));
       await uploadZip('/fake/file/path.zip').should.eventually.be.rejectedWith(/Could not find/);
+      uploaderStub.restore();
     });
     it('should reject if s3.upload fails', async () => {
-      const uploaderStub = sinon.stub(s3Proto, 'upload', (obj, cb) => { cb(new Error('does not matter')); });
+      const fakeEventObject = {
+        on: () => {},
+      };
+      uploaderStub = sinon.stub(s3Proto, 'upload', ((obj, cb) => {
+        cb(new Error('some random S3 error'));
+        return fakeEventObject;
+      }));
       await uploadZip('/fake/file/path.zip').should.eventually.be.rejectedWith(/Could not upload/);
       uploaderStub.restore();
     });
     it('should pass if s3.upload does not fail', async () => {
-      const uploaderStub = sinon.stub(s3Proto, 'upload', (obj, cb) => { cb(null, 'Success'); });
-      await uploadZip().should.eventually.equal('Success');
+      const fakeEventObject = {
+        on: () => {},
+      };
+      uploaderStub = sinon.stub(s3Proto, 'upload', ((obj, cb) => {
+        cb(null, 'Success');
+        return fakeEventObject;
+      }));
+      s3FileExistsStub.returns(false);
+      await uploadZip('/fake/file/path.zip').should.eventually.be.resolved;
       uploaderStub.restore();
     });
-  });
+    it('should not reupload if it is cached', async () => {
+      const fakeEventObject = {
+        on: () => {},
+      };
 
-  describe('deleteZip', () => {
-    it('should reject if AWS_S3_BUCKET not defined in env', async () => {
-      const backupBucket = process.env.AWS_S3_BUCKET;
-      delete process.env.AWS_S3_BUCKET;
-      await deleteZip().should.eventually.be.rejectedWith(/AWS_S3_BUCKET/);
-      process.env.AWS_S3_BUCKET = backupBucket;
-    });
-    it('should reject if s3.deleteObject throws error', async () => {
-      const deleteObjectStub = sinon.stub(s3Proto, 'deleteObject', (obj, cb) => { cb(new Error('does not matter')); });
-      await deleteZip().should.eventually.be.rejectedWith(/Could not delete/);
-      deleteObjectStub.restore();
-    });
-    it('should resolve if s3.deleteObject does not fail', async () => {
-      const deleteObjectStub = sinon.stub(s3Proto, 'deleteObject', (obj, cb) => { cb(null, 'Success'); });
-      await deleteZip().should.eventually.equal('Success');
-      deleteObjectStub.restore();
+      // Shouldn't matter if s3.upload fails
+      uploaderStub = sinon.stub(s3Proto, 'upload', ((obj, cb) => {
+        cb(new Error('some random S3 error'));
+        return fakeEventObject;
+      }));
+      s3FileExistsStub.returns(true);
+      await uploadZip('/fake/file/path.zip').should.eventually.be.resolved;
     });
   });
 });
