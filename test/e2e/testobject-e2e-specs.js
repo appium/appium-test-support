beafd1993d9c1ecff4d7ca312e13c47d279e141c
Dan Graham <dan@saucelabs.com>
2017-11-10 09:53:07 -0800

    Fetch appium fixes (#9)
    * 0.6.0
    
    * Don't accept 'git@' URL for package.json rewrite
    
    * Use cross-platform zip
    
    * Squash
    
    * squash
    
    * PR fixes
    
    * Use archiver instead of bestzip
    
    * Bestzip was timing out
    * Also added a 'COMMIT_SHA' field to fetchAppium that adds the SHA to the zip filename to make it unique
    
    * Use sha in git url
    
    * Fix commit sha
    
    * Fix
    
    * fix

--- test/e2e/testobject-e2e-specs.js
+++ test/e2e/testobject-e2e-specs.js
@@ -12,12 +12,12 @@ chai.should();
 chai.use(chaiAsPromised);
 
 describe('TestObject', function () {
-  describe('#fetchAppium', function () {
+  describe('#fetchAppium @skip-ci', function () {
     it('fetches appium zip', async function () {
       const appiumZip = await fetchAppium(
         'appium-uiautomator2-driver',
         'git+https://git@github.com/appium/appium-uiautomator2-driver.git',
-        'master'
+        'master',
       );
       await fs.exists(appiumZip).should.eventually.be.true;
     });
@@ -34,7 +34,12 @@ describe('TestObject', function () {
       const s3Proto = Object.getPrototypeOf(new AWS.S3());
       const s3UploadSpy = sinon.spy(s3Proto, 'upload');
       s3Proto.upload.notCalled.should.be.true;
-      const wdObject = await enableTestObject(wd, 'appium-uiautomator2-driver', 'git+https://git@github.com/appium/appium-uiautomator2-driver.git');
+      const wdObject = await enableTestObject(
+        wd,
+        'appium-uiautomator2-driver',
+        'git+https://git@github.com/appium/appium-uiautomator2-driver.git',
+        'master',
+      );
       s3Proto.upload.callCount.should.be.below(2);
 
       // Test that the zip was uploaded
