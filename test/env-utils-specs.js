af98731aa911fd54b8196d22ce112a7e4a363d18
dan@saucelabs.com <dan@saucelabs.com>
2018-03-26 17:23:23 -0700

    Mocha arrows fix

--- test/env-utils-specs.js
+++ test/env-utils-specs.js
@@ -7,15 +7,15 @@ import chai from 'chai';
 chai.should();
 const expect = chai.expect;
 
-describe('env-utils', () => {
-  describe('stubEnv', () => {
+describe('env-utils', function () {
+  describe('stubEnv', function () {
     stubEnv();
 
-    it('setting env variable', () => {
+    it('setting env variable', function () {
       process.env.ABC = 'abc';
     });
 
-    it('env varible should not be set', () => {
+    it('env varible should not be set', function () {
       expect(process.env.ABC).not.to.exist;
     });
   });
