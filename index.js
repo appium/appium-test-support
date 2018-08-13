f8868400c3b865650e96667f2b4e7bdd4436e3f8
dan@saucelabs.com <dan@saucelabs.com>
2018-08-13 12:46:30 -0700

    Remove S3 and AWS

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import { stubLog } from './lib/log-utils';
 import { fakeTime } from './lib/time-utils';
 import { withMocks, verifyMocks } from './lib/mock-utils';
 import { withSandbox, verifySandbox } from './lib/sandox-utils';
-import { enableTestObject, disableTestObject } from './lib/testobject';
 
 export { stubEnv, stubLog, fakeTime, withSandbox, verifySandbox, withMocks,
-         verifyMocks, enableTestObject, disableTestObject };
+         verifyMocks };
