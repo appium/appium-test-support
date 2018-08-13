024d827f3e7af941ad9410b7fb1c85e541da0d99
Isaac Murchie <isaac@saucelabs.com>
2018-08-13 15:52:09 -0400

    Turn on unhandled promise rejection handling

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import { fakeTime } from './lib/time-utils';
 import { withMocks, verifyMocks } from './lib/mock-utils';
 import { withSandbox, verifySandbox } from './lib/sandbox-utils';
 
+// this just needs to be imported, for the functionality to be injected
+import './lib/unhandled-rejection';
 
 export {
   stubEnv, stubLog, fakeTime, withSandbox, verifySandbox, withMocks, verifyMocks
