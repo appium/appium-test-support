69b1e21ea94aaafcade8e93aef407de3d895fa03
Mykola Mokhnach <nick+1@wire.com>
2018-08-27 14:11:15 +0200

    Move timeout to a variable

--- bin/android-emu-travis-post.sh
+++ bin/android-emu-travis-post.sh
@@ -6,15 +6,16 @@ if [ ${START_EMU} = "1" ]; then
     android-wait-for-emulator
 
     seconds_elapsed=0
-    while [[ $seconds_elapsed -lt 60 ]]; do
+    timeout=60
+    while [[ $seconds_elapsed -lt $timeout ]]; do
         pm_state=`adb shell pm get-install-location 2>&1 || true`
         echo "$pm_state" | grep -Eq "\d+\[\w+\]" && break || true
         echo "Waiting for emulator to finish services startup"
         sleep 1
         let "seconds_elapsed += 1"
     done
-    if [[ $seconds_elapsed -ge 60 ]]; then
-        echo "Timeout of 60 seconds reached; failed to start emulator services"
+    if [[ $seconds_elapsed -ge $timeout ]]; then
+        echo "Timeout of $timeout seconds reached; failed to start emulator services"
         exit 1
     fi
 
