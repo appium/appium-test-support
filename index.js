01700613c283faeeffabaae1f88fb7c61a05820f
Isaac Murchie <isaac@saucelabs.com>
2018-10-04 20:11:38 -0400

    Rename sandbox-utils file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { stubEnv } from './lib/env-utils';
 import { stubLog } from './lib/log-utils';
 import { fakeTime } from './lib/time-utils';
 import { withMocks, verifyMocks } from './lib/mock-utils';
-import { withSandbox, verifySandbox } from './lib/sandox-utils';
+import { withSandbox, verifySandbox } from './lib/sandbox-utils';
 
 
 export {
