70b95ab64fb0160cfbb5bf8ba969f5e5f5673782
Mykola Mokhnach <mokhnach@gmail.com>
2019-05-01 21:11:00 +0200

    Make emulator startup timeout configurable

--- bin/android-emu-travis-post.sh
+++ bin/android-emu-travis-post.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+: ${EMU_STARTUP_TIMEOUT:=360}
+
 if [ ${START_EMU} = "1" ]; then
     # Fail fast if emulator process cannot start
     pgrep -nf avd || exit 1
@@ -7,8 +9,7 @@ if [ ${START_EMU} = "1" ]; then
     # make sure the emulator is ready
     adb wait-for-device get-serialno
     secondsStarted=`date +%s`
-    TIMEOUT=360
-    while [[ $(( `date +%s` - $secondsStarted )) -lt $TIMEOUT ]]; do
+    while [[ $(( `date +%s` - $secondsStarted )) -lt $EMU_STARTUP_TIMEOUT ]]; do
         processList=`adb shell ps`
         if [[ "$processList" =~ "com.android.systemui" ]]; then
             echo "System UI process is running. Checking IME services availability"
@@ -16,10 +17,14 @@ if [ ${START_EMU} = "1" ]; then
         fi
         sleep 5
         secondsElapsed=$(( `date +%s` - $secondsStarted ))
-        secondsLeft=$(( $TIMEOUT - $secondsElapsed ))
+        secondsLeft=$(( $EMU_STARTUP_TIMEOUT - $secondsElapsed ))
         echo "Waiting until emulator finishes services startup; ${secondsElapsed}s elapsed; ${secondsLeft}s left"
     done
     bootDuration=$(( `date +%s` - $secondsStarted ))
+    if [[ $bootDuration -ge $EMU_STARTUP_TIMEOUT ]]; then
+        echo "Emulator has failed to fully start within ${EMU_STARTUP_TIMEOUT}s"
+        exit 1
+    fi
     echo "Emulator booting took ${bootDuration}s"
 
     adb shell input keyevent 82
